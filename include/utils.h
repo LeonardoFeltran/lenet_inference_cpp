f02c36902828502ba388584c022044e83ec1aec1
Leonardo Feltran <leonardo.feltran@gmail.com>
2020-12-18 15:27:22 +0100

    Correct error in ReLU activation

--- include/utils.h
+++ include/utils.h
@@ -6,19 +6,36 @@ typedef float LENET_T;
 /**
  * Add zero padding on the input
  *
- * @param[in] input:		Input image
- * @param[out] output:		Padded image
+ * @param[in] input:	Input image
+ * @param[out] output:	Padded image
  *
 */
-void padding(LENET_T in[28][28], LENET_T out[32][32]);
+void padding (LENET_T in[28][28], LENET_T out[32][32]);
 
 /**
  * Calculate the activation value using the ReLU function
  *
- * @param[in] input:		Input value
- * @param[out] output:		Activation value
+ * @param[in] input:	Input value
+ * @param[out] output:	Activation value
  *
 */
-void relu_activation(LENET_T input, LENET_T output);
+LENET_T relu_activation (LENET_T input);
+
+/**
+ * Read weights from a stream and store them into a tensor
+ *
+ * @param[in] weights_stream:	Stream with all weights queued 
+ * @param[out] weights_tensor: 	Tensor to store all weights
+*/
+void read_weights (LENET_T* weights_stream, LENET_T weights_tensor[5][5][1][6]);
+
+/**
+ * Read all biases from a stream and store them into a tensor
+ *
+ * @param[in] bias_stream:	Stream with all biases queued 
+ * @param[out] bias_tensor: Tensor to store all biases
+*/
+void read_biases (LENET_T* weights_stream, LENET_T weights_tensor[6]);
+
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -15,6 +15,27 @@ void padding (LENET_T in[28][28], LENET_T out[32][32]) {
 }
 
 
-void relu_activation(LENET_T input, LENET_T output){
-	output = input < 0 ? 0 : input;
+LENET_T relu_activation(LENET_T input){
+	// Check if the input id negative
+	// If true, the output is zero else it receives the input
+	return input < 0 ? (LENET_T)0 : input;
+}
+
+void read_weights (LENET_T* weights_stream, LENET_T weights_tensor[5][5][1][6]){
+	//Iterate over the output tensor to write all values from the input
+	Kernel_row: for (int kr = 0; kr < 5; kr++){
+		Kernel_col: for (int kc = 0; kc < 5; kc++){
+			I_Feature_Map: for (int ifm = 0; ifm < 1; ifm++){
+				O_Feature_Map: for (int ofm = 0; ofm < 6; ofm++){
+					weights_tensor[kr][kc][ifm][ofm] = weights_stream[ofm + ifm*6 * kc*30 + kr*150];
+				}
+			}
+		}
+	}
+}
+
+void read_biases (LENET_T* weights_stream, LENET_T weights_tensor[6]){
+	Read_bias: for (int i = 0; i < 6; i++){
+		weights_tensor[i] = weights_stream[i];
+	}
 }
