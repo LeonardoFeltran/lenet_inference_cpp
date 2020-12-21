0e18877fa8a2acc5f0d7ab5689e9e307504246c4
Leonardo Feltran <leonardo.feltran@gmail.com>
2020-12-21 17:24:52 +0100

    Change functions used to read weights and biases

--- src/utils.cpp
+++ src/utils.cpp
@@ -18,24 +18,34 @@ void padding (LENET_T in[28][28], LENET_T out[32][32]) {
 LENET_T relu_activation(LENET_T input){
 	// Check if the input id negative
 	// If true, the output is zero else it receives the input
-	return input < 0 ? (LENET_T)0 : input;
+	return input < 0 ? (LENET_T) 0 : input;
 }
 
-void read_weights (LENET_T* weights_stream, LENET_T weights_tensor[5][5][1][6]){
-	//Iterate over the output tensor to write all values from the input
-	Kernel_row: for (int kr = 0; kr < 5; kr++){
-		Kernel_col: for (int kc = 0; kc < 5; kc++){
-			I_Feature_Map: for (int ifm = 0; ifm < 1; ifm++){
-				O_Feature_Map: for (int ofm = 0; ofm < 6; ofm++){
-					weights_tensor[kr][kc][ifm][ofm] = weights_stream[ofm + ifm*6 * kc*30 + kr*150];
-				}
-			}
-		}
+void read_params_conv (int kernel_num, int kernel_size, int feature_maps_input,
+					  LENET_T weights_stream[61470], LENET_T bias_stream[236],
+					   LENET_T* weights_buffer, LENET_T* bias_buffer){
+	//Calculate the number of weigths to read
+	int num_weights = kernel_num * kernel_size * kernel_size * feature_maps_input;
+	//Reads all weights and store on internal buffers
+	Read_weights_conv: for (int i = 0; i < num_weights; i++){
+		weights_buffer[i] = weights_stream[i];
+	}
+	//Reads all biases and store on internal buffers
+	Read_bias_conv: for (int p = 0; p < kernel_num; p++){
+		bias_buffer[p] = bias_stream[p];
 	}
 }
 
-void read_biases (LENET_T* weights_stream, LENET_T weights_tensor[6]){
-	Read_bias: for (int i = 0; i < 6; i++){
-		weights_tensor[i] = weights_stream[i];
+void read_params_dense (int input_size, int neurons_num, LENET_T weights_stream[61470], LENET_T bias_stream[236],
+					   LENET_T* weights_buffer, LENET_T* bias_buffer){
+	//Address to read and store the values
+	int num_weights = input_size * neurons_num;
+	//Reads all weights and store on internal buffers
+	Read_weights_dense: for (int i = 0; i < num_weights; i++){
+		weights_buffer[i] = weights_stream[i];
+	}
+	//Reads all biases and store on internal buffers
+	Read_bias_dense: for (int p = 0; p < neurons_num; p++){
+		bias_buffer[p] = bias_stream[p];
 	}
 }
